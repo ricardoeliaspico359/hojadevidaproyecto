aee131b5c18f9aaf79e22530913d1205785de91a
ricardoeliaspico359 <ricardoeliaspico359@gmail.com>
2026-01-11 22:43:04 -0500

    crear superusuario automatico en render

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+# --- 4. CREAR SUPERUSUARIO DESDE CERO ---
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='Dayana').exists() or \
+User.objects.create_superuser('Dayana', 'Dayana@gmail.com', '123')" \
+| python manage.py shell
