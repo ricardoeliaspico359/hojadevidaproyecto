c677c6aa9f66c59e7a20c72a66d3b40579aca40c
ricardoeliaspico359 <ricardoeliaspico359@gmail.com>
2026-01-11 21:09:07 -0500

    mensaje claro

--- cv_project/urls.py
+++ cv_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from perfil import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('perfil.urls')),  # Dashboard como página principal
